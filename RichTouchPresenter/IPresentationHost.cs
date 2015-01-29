2dcd6b60a6c41e5f12debcaaac14a54fab8172ea
Joe Cooper <joe@artofbytes.com>
2015-01-29 23:19:40 +0100

    First implementation

---
+++ RichTouchPresenter/IPresentationHost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UIKit;
+using Cirrious.MvvmCross.Touch.Views;
+
+namespace NobleMuffins.RichTouchPresenter
+{
+	public interface IPresentationHost
+	{
+		bool ShouldPresentViewController(IMvxTouchView view);
+		void PresentViewController(IMvxTouchView view, out Action dismissalAgent);
+	}
+}
+
---
+++ RichTouchPresenter/TouchPresenter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Cirrious.MvvmCross.Touch.Views.Presenters;
+using Cirrious.MvvmCross.Touch.Platform;
+using UIKit;
+using Cirrious.MvvmCross.Touch.Views;
+using System.Collections.Generic;
+
+namespace NobleMuffins.RichTouchPresenter
+{
+	public class TouchPresenter: MvxTouchViewPresenter
+	{
+		public TouchPresenter (MvxApplicationDelegate appDelegate, UIWindow window)
+			: base(appDelegate, window)
+		{
+		}
+
+		private readonly ICollection<IPresentationHost> presentationHosts = new HashSet<IPresentationHost>();
+		private readonly IDictionary<object,Action> removalAgentsByViewModel = new Dictionary<object,Action>();
+
+		public override void Show (Cirrious.MvvmCross.Touch.Views.IMvxTouchView view)
+		{
+			if (view is IPresentationHost) {
+				presentationHosts.Add ((IPresentationHost) view);
+			}
+
+			bool presentedByHost = false;
+
+			foreach (var host in presentationHosts) {
+				var doThisOne = host.ShouldPresentViewController (view);
+				if (doThisOne) {
+					Action dismissalAgent = null;
+					host.PresentViewController (view, out dismissalAgent);
+					if (dismissalAgent == null) {
+						throw new NullReferenceException ("IPresentationHost.PresentViewController must yield a dismissal agent if .ShouldPresentViewController yields true.");
+					}
+					removalAgentsByViewModel [view.ViewModel] = dismissalAgent;
+					presentedByHost = true;
+				}
+				if (presentedByHost) {
+					break;
+				}
+			}
+
+			if (!presentedByHost) {
+				base.Show (view);
+			}
+		}
+
+		public override void Close (Cirrious.MvvmCross.ViewModels.IMvxViewModel toClose)
+		{
+			if (toClose is IPresentationHost && presentationHosts.Contains ((IPresentationHost) toClose)) {
+				presentationHosts.Remove ((IPresentationHost) toClose);
+			}
+			if (removalAgentsByViewModel.ContainsKey (toClose)) {
+				removalAgentsByViewModel [toClose] ();
+				removalAgentsByViewModel.Remove (toClose);
+			} else {
+				base.Close (toClose);
+			}
+		}
+	}
+}
+
